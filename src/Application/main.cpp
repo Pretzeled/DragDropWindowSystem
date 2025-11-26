25710b26c574106100670166f14d31dc8846d029
Pretzeled <ezrakahn1@gmail.com>
2025-11-25 22:47:43 -0800

    Created basic application layout

--- src/Application/main.cpp
+++ src/Application/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <QApplication>
+#include <QTabWidget>
+#include <QTextEdit>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello, World!" << std::endl;
-
-    return 0;
+    QApplication app(argc, argv);
+    QTabWidget tabWidget;
+    for (int i = 1; i <= 3; ++i) {
+        auto* edit = new QTextEdit();
+        tabWidget.addTab(edit, QString("Tab %1").arg(i));
+    }
+    tabWidget.show();
+    return app.exec();
 }
